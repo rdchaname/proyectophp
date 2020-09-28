1cfd713d87202f60f2a4f32c0cf91a9e72b9eca6
Rubén Chanamé Sánchez <rdchaname@gmail.com>
2020-09-28 12:53:34 -0500

    Versión sin laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Persona;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,36 +16,3 @@ use Illuminate\Support\Facades\DB;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/persona', function () {
-    // REGISTRAR
-    // $persona = new Persona();
-    // $persona->nombre = 'Juan';
-    // $persona->apellido_paterno = 'Perez';
-    // $persona->apellido_materno = 'Suarez';
-    // $persona->email = 'jperez2@gmail.com';
-    // $persona->save();
-    // ACTUALIZAR
-    // $persona = Persona::find(3);
-    // $persona->apellido_paterno = 'Gonzales';
-    // $persona->save();
-    // ELIMINAR
-    // $persona = Persona::find(4);
-    // $persona->delete();
-    // CONSULTA
-    // $personas = Persona::all();
-    $personas = Persona::where('name', '=', 'Jose')
-        ->select('apellido_paterno', 'apellido_materno')
-        ->orderBy('nombre', 'DESC')
-        ->get();
-
-    // DB::raw("SELECT procedimient(".$personas.")");
-    // foreach($personas as $persona){
-    //     echo $persona->apellido_paterno."<br>";
-    // }
-    echo "Ruta persona";
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
